e82fc884cbfbfbd7123f303eb39a0a26bba7da23
sinncodes <thecrysispl@gmail.com>
2026-03-25 22:16:34 +0000

    Adding models

---
+++ shows/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Show(models.Model):
+    name = models.CharField(max_length=50)#increase max length if any title gets cropped
+    genres = models.ManyToManyField(Genre)
+    rating = models.IntegerField()
+     # pg - make this into a choices via pg rating stuff
+    release_date = models.DateField()
+ 
+ #class Interaction(models.Model):
+ #   interactions = m
